64cf49599bf0cc8eb6964115bc604a3c91ce74db
Stevan Little <stevan.little@booking.com>
2016-05-27 21:00:36 +0200

    some quick docs and notes

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# PPIx::Statement::Sub::Signature
+
+Currently PPI does not have a way to handle the new
+subroutine signature feature in Perl 5.22, this module
+attempts to (kind of) fix this.
--- lib/PPIx/Statement/Sub/Signature.pm
+++ lib/PPIx/Statement/Sub/Signature.pm
@@ -12,8 +12,20 @@ sub new {
     (blessed $sub && $sub->isa('PPI::Statement::Sub'))
         || confess 'You must pass an instance of PPI::Statement::Sub, not ' . $sub;
 
+    # NOTE:
+    # the `prototype` seems to get spaces between
+    # the commas removed, this might prove an issue
+    # with more complex signatures.
+    # - SL
+
     my $proto  = $sub->prototype =~ s/^\(//r =~ s/\)$//r;
     my @params = split /\s*\,\s*/ => $proto;
+
+    # TODO:
+    # the above "parsing" does not account for
+    # the possibility of default values in the
+    # signature, this needs to be added.
+    # - SL
 
     bless {
         _sub    => $sub,
@@ -41,5 +53,17 @@ sub parameter_names {
 __END__
 
 =pod
+
+=head1 NAME
+
+PPIx::Statement::Sub::Signature
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+Currently L<PPI> does not have a way to handle the new
+subroutine signature feature in Perl 5.22, this module
+attempts to (kind of) fix this.
 
 =cut
